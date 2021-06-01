e8ec4bbb84d63e2794d0d12fc058e949c06b6182
Joshua <mcabej12@gmail.com>
2021-06-01 11:35:09 +0100

    Updates

--- src/assets/images/fiction/fiction.js
+++
@@ -1,7 +0,0 @@
-const FictionImages = {
-  harryPotter: require("./harry-potter.jpg").default,
-  lordOfTheFlies: require("./lord-of-the-flies.jpg").default,
-  pridePrejudice: require("./pride-prejudice.jpg").default,
-};
-
-export default FictionImages;
